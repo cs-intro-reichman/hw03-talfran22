115ca5d1ed748c9486ab2057cf9ce5fd6ed99ad2
Tal Frankenthal <154238085+talfran22@users.noreply.github.com>
2024-01-02 15:31:52 +0200

    update

--- LoanCalc.java
+++ LoanCalc.java
@@ -16,6 +16,7 @@ public class LoanCalc {
 		double loan = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
+		double payment = Integer.parseInt(args[3]);
 		System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 		
 		// Computes the periodical payment using brute force search
@@ -39,8 +40,13 @@ public class LoanCalc {
 	*/
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
+    	double payment = loan/n;
+    	iterationCounter = 0;
+    	while(endBalance(loan, rate, n, payment) > epsilon){
+    		payment += epsilon;
+    		iterationCounter++;
+    	}
+    	return payment;
     }
     
     /**
@@ -51,8 +57,21 @@ public class LoanCalc {
 	*/
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
+    	double l = loan / n;
+    	double h = loan;
+    	double payment = ((h - l) / 2);
+    	iterationCounter = 0;
+    	while(Math.abs(endBalance(loan, rate, n, payment)) >= epsilon){
+    		if (endBalance(loan, rate, n, payment) > epsilon){
+				l = payment;
+    		}
+			else{
+				h = payment;
+			}
+			payment = (l + h) / 2;
+			iterationCounter++;
+    	}
+    	return payment;
     }
 	
 	/**
@@ -60,7 +79,10 @@ public class LoanCalc {
 	* interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	*/
 	private static double endBalance(double loan, double rate, int n, double payment) {
-		// Replace the following statement with your code
-    	return 0;
+		for (int i = 1; i <= n ; i++ ) {
+			loan -= payment;
+			loan = loan * (1 + (rate / 100));
+		}
+		return loan;
 	}
 }

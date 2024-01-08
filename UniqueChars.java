29ec7232d5c724309f5cee5c6ebbd161356f4532
Tal Frankenthal <154238085+talfran22@users.noreply.github.com>
2024-01-08 18:53:06 +0200

    Update UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -3,7 +3,7 @@ public class UniqueChars {
     public static void main(String[] args) {  
         String str = args[0];
         System.out.println(uniqueChars(str));
-                System.out.println(uniqueChars("yael played the yokelele"));
+        
 
     }
 
